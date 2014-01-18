b3f45c55ace8520e1938a0841adb1a4ff53a2639
sowdust <sowdust@gmail.com>
2014-01-19 00:01:35 +0100

    first

--- common/config.php
+++ common/config.php
@@ -2,10 +2,10 @@
 
 class config {
 
-	const basedir = 'tweb/';
+	const basedir = 'mvc/';
 	const basehost = 'http://localhost/';
-	const nomesito = 'bekele90';
-	const serverpath = '/var/www/tweb/';
+	const nomesito = 'MVC';
+	const serverpath = '/var/www/mvc/';
 	const user_img = 'usr_img/';
 }
 
